9f3f803e106a431f4414050e91055f9502da5d1a
sathwikamolakalapalli123 <bhuvana.molakalapalli@gmail.com>
2025-12-29 12:24:22 +0530

    Add buyNow and placeOrder functions

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyNow(product) {
+  alert(product + " added to checkout");
+  document.getElementById("checkout").scrollIntoView();
+}
+
+function placeOrder() {
+  alert("Order placed successfully!\nThank you for shopping with us.");
+}
